d38223799c40cbaeb2b49f1785e24ccdedf7f49c
yuval tarnopolsky <yuvaltar1337@gmail.com>
2022-12-19 16:32:30 +0200

    random stuf.py

---
+++ random_stuuf.py
@@ -0,0 +1,24 @@
+'''def is_Power_of_four(n):
+    while n > 1 and (n%4) == 0:
+        n /=4
+    return n == 1
+    
+
+n = int(input('number'))
+print(is_Power_of_four(n))
+'''
+
+# def is_Power_of_four(n):
+#     while n and not (n & 0b11):
+#         n >>= 2
+#     return (n == 1)
+
+# print(is_Power_of_four(4))
+
+
+# def is_Power_of_two():
+#         n = int(input('write number'))
+#         return n > 0 and (n & (n - 1)) == 0
+# print(str(is_Power_of_two()))
+print(bin(3))
+
